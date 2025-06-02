8e6a129fb98e505d69066142a3b021c402f07c24
rubendcdev <hugorubendc.developer@gmail.com>
2025-06-01 19:45:02 -0600

    Primer commit del proyecto

---
+++ src/main/java/com/softhugordc/githubflowpractica/GithubflowPracticaApplication.java
@@ -0,0 +1,13 @@
+package com.softhugordc.githubflowpractica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubflowPracticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubflowPracticaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softhugordc/githubflowpractica/GithubflowPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softhugordc.githubflowpractica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubflowPracticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
